10098852df4b1b54deb261fe77ed380f4ead6e6f
NaoTsutomu <iamdez96@gmail.com>
2024-03-21 21:02:35 +0800

    added upcoming table view

---
+++ Netflix Clone/View Models/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Ana Naomi Erlandez on 3/21/24.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
