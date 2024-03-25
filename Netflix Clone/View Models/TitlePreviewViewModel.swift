57d597ce74518cd44f47c37d163498041e7e2df5
NaoTsutomu <iamdez96@gmail.com>
2024-03-25 20:47:11 +0800

    Create TitlePreviewViewModel.swift

---
+++ Netflix Clone/View Models/TitlePreviewViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  TitlePreviewViewModel.swift
+//  Netflix Clone
+//
+//  Created by Ana Naomi Erlandez on 3/25/24.
+//
+
+import Foundation
+
+
+struct TitlePreviewViewModel {
+    let title: String
+    let youtubeVideo: VideoElement
+    let titleOverview: String
+}
